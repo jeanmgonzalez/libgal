f5d8df7b368cf2cb508b270caadfa80a7abfac69
Jean González <ebrainding@gmail.com>
2023-08-26 11:41:50 -0300

    Se agregaron funciones de Machine Learning

--- libgal.py
+++ libgal.py
@@ -35,6 +35,15 @@ try:
     from sqlalchemy.orm import sessionmaker
     from sqlalchemy.ext.declarative import declarative_base
     from sqlalchemy.exc import OperationalError as SQLAlchemyError
+
+    #Machine Learning
+    from sklearn.base import BaseEstimator, TransformerMixin
+    import pandas as pd
+    import numpy as np
+    from collections import defaultdict
+    from scipy.stats import ks_2samp
+    from sklearn.metrics import roc_curve, roc_auc_score
+
     
 except ImportError as imp_err:
     # Freno ejecucion y devuelvo codigo de error
@@ -159,6 +168,16 @@ def teradata(host,user,password,logmech="LDAP"):
 
 class sqlalchemy:
     
+    """
+    Descripción: Permite la conexion hacia el sgdb de 
+    Parámetro:
+    - driver (String): Tipo de conexión o base de datos a utilizar
+    - host (String): uri del servidor de base de datos
+    - user (String): Usuario que autentica la conexión a la base de datos
+    - password (String): Contraseña para la autenticación de la connexión de la base de datos
+    - logmech (String): Parámetro Opcional que indica el método de autenticación del usuario. LDAP por defecto
+    """
+    
     def __init__(self, driver, host, user, password, logmech="LDAP"):
     
         if driver.lower()=="teradata":
@@ -177,3 +196,112 @@ class sqlalchemy:
     def query(self, query):
         with self.engine.connect() as conn:
             return conn.execute(text(query))
+
+####FUNCIONES DE MACHINE LEARNING
+
+def evaluate_ks_and_roc_auc(y_real, y_proba):
+    # Unite both visions to be able to filter
+    df = pd.DataFrame()
+    df['real'] = y_real
+    df['proba'] = y_proba
+    class0 = df[df['real'] == 0]
+    class1 = df[df['real'] == 1]
+    ks = ks_2samp(class0['proba'], class1['proba'])
+    roc_auc = roc_auc_score(df['real'] , df['proba'])
+    
+    print(f"KS: {ks.statistic:.4f} (p-value: {ks.pvalue:.3e})")
+    print(f"ROC AUC: "+str(roc_auc))
+    return ks.statistic, roc_auc, df
+
+class NumNormTransformer(BaseEstimator, TransformerMixin):
+    #the constructor
+    def __init__(self, keep_original = True, sufix = ['vl'],exclude=['periodo_cd','cd_periodo']):
+        self.keep_original = keep_original
+        self.sufix = sufix
+        self.exclude=exclude
+
+    #estimator method
+    def fit(self, X,y=None):
+        return self
+    #transfprmation
+    def transform(self, X,y=None):
+        self.originals = []
+        for col in X.columns:
+            for suf in self.sufix:
+                if (col.endswith(suf) or col.startswith(suf)) and col not in self.exclude:
+                    self.originals.append(col)
+        
+        self.X_norm = X[self.originals]
+        self.X_norm.columns = [str(x)+"_norm" for x in self.X_norm]
+        self.X_norm = (self.X_norm-self.X_norm.mean()) / (self.X_norm.std()) 
+        
+        
+        
+        self.X_sca= X[self.originals]
+        self.X_sca.columns = [str(x)+"_sca" for x in self.X_sca]
+        self.X_sca = (self.X_sca.max()-self.X_sca)/(self.X_sca.max()-self.X_sca.min())        
+        X.drop(columns = self.originals, inplace = True)
+        
+        X = pd.concat([X, self.X_norm], axis = 1)
+        return X
+
+class NumLogTransformer(BaseEstimator, TransformerMixin):
+    #the constructor
+    def __init__(self, keep_original = True, sufix = ['vl'],exclude=['periodo_cd','cd_periodo']):
+        self.keep_original = keep_original
+        self.sufix = sufix
+        self.exclude=exclude
+
+    #estimator method
+    def fit(self, X,y=None):
+        return self
+    #transfprmation
+    def transform(self, X,y=None):
+        self.originals = []
+        for col in X.columns:
+            for suf in self.sufix:
+                if (col.endswith(suf) or col.startswith(suf)) and col not in self.exclude:
+                    self.originals.append(col)
+        
+        self.X_log = X[self.originals]
+        self.X_log.columns = [str(x)+"_log" for x in self.X_log]
+        self.X_log = np.log(self.X_log+1)
+        X = pd.concat([X, self.X_log], axis = 1)
+        if not self.keep_original:
+            X.drop(columns = self.originals, inplace = True)
+        return X
+
+class CategoricalReduceTransformer(BaseEstimator, TransformerMixin):
+    #the constructor
+    def __init__(self, keep_original = False, sufix = ['tx','cd'], threshold = 0.99,exclude=['periodo_cd','cd_periodo']):
+        self.keep_original = keep_original
+        self.sufix = sufix
+        self.threshold=threshold
+        self.exclude=exclude
+    #estimator method
+    def fit(self, X,y=None):
+        return self
+    #transfprmation
+    def transform(self, X,y=None):
+        self.originals = []
+        for col in X.columns:
+            for suf in self.sufix:
+                if (col.endswith(suf) or col.startswith(suf)) and col not in self.exclude:
+                    self.originals.append(col)                
+                    D = X.groupby([col],as_index=False).size().rename(columns={'size':'n'}).sort_values('n',ascending = False)            
+                    D['p'] = D['n']/D['n'].sum()
+                    D['cumsum_p']=D['p'].cumsum()
+                    dict_estados = defaultdict(lambda:'Otros')
+                    for i in range(len(D)):
+                        if D.cumsum_p[i]<self.threshold:
+                            dict_estados[D[col][i]] = str(D[col][i]).replace(".","").replace("°","").replace(">"," ").replace("<"," ").replace("/"," ")
+                        else:
+                            dict_estados[D[col][i]] = "Otros"
+                    if self.keep_original:
+                        X["freq_"+str(col)] = [dict_estados[x] for x in X[col]]
+                    else:
+                        X[str(col)] = [dict_estados[x] for x in X[col]]
+            
+
+
+        return X  
